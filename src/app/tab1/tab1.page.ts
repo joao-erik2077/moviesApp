0a66ff61bb7d05ae82c1116f37e8ba33d2421459
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-08-07 16:18:40 -0300

    Tab1 and Tab2 design

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,115 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  @ViewChild(IonModal) modal: IonModal;
+
+  public filmesPrincipal = [
+    {
+      nome: 'Minions 2: A Origem de Gru',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tzFAboMUGJKoPQEtlxfxbbYsSWa.jpg',
+      ano: 2022,
+      categorias: ['Família', 'Animação', 'Aventura', 'Comédia', 'Fantasia'],
+      avaliacao: 78
+    },
+    {
+      nome: 'Homem-Aranha: Sem Volta Para Casa',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fVzXp3NwovUlLe7fvoRynCmBPNc.jpg',
+      ano: 2021,
+      categorias: ['Ação', 'Aventura', 'Ficção científica'],
+      avaliacao: 80
+    },
+    {
+      nome: 'Sonic 2: O Filme',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f4SvCKIUrC2cDR7Xo4k1kaGAqQ2.jpg',
+      ano: 2022,
+      categorias: ['Ação', 'Aventura', 'Família', 'Comédia'],
+      avaliacao: 77
+    }
+  ];
+
+  public filmesRoleta1 = [
+    {
+      nome: 'Morbius',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg',
+      ano: 2022,
+      categorias: ['Ação', 'Ficção científica', 'Fantasia'],
+      avaliacao: 65
+    },
+    {
+      nome: 'Demon Slayer - Mugen Train: O Filme',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h8Rb9gBr48ODIwYUttZNYeMWeUU.jpg',
+      ano: 2020,
+      categorias: ['Animação', 'Ação', 'Aventura', 'Fantasia'],
+      avaliacao: 84
+    },
+    {
+      nome: 'O Poderoso Chefinho 2: Negócios da Família',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/p033k6k8V9USI0TWZavIxoYzsGi.jpg',
+      ano: 2021,
+      categorias: ['Animação', 'Comédia', 'Aventura', 'Família'],
+      avaliacao: 75
+    },
+    {
+      nome: 'Larva: O Pingente Mágico',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jy2LLGv4ORGp72XSAUFVcVJCWxQ.jpg',
+      ano: 2022,
+      categorias: ['Comédia', 'Família', 'Animação'],
+      avaliacao: 70
+    },
+  ];
+
+  public filmesRoleta2 = [
+    {
+      nome: 'Um Sonho de Liberdade',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/umX3lBhHoTV7Lsci140Yr8VpXyN.jpg',
+      ano: 1994,
+      categorias: ['Drama', 'Crime'],
+      avaliacao: 87
+    },
+    {
+      nome: 'A Viagem de Chihiro',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/e7WdOF6j3wB5kFXIEoqGXKmGaTl.jpg',
+      ano: 2001,
+      categorias: ['Animação', 'Família', 'Fantasia'],
+      avaliacao: 85
+    },
+    {
+      nome: 'Três Homens em Conflito',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/f6CoXpyZRqYJF3lTfxjWIahO6qM.jpg',
+      ano: 1966,
+      categorias: ['Faroeste'],
+      avaliacao: 85
+    },
+    {
+      nome: 'Clube da Luta',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/r3pPehX4ik8NLYPpbDRAh0YRtMb.jpg',
+      ano: 1999,
+      categorias: ['Drama'],
+      avaliacao: 84
+    },
+  ];
+
+  public filmeModal = {
+    nome: '',
+    foto: '',
+    ano: 0,
+    categorias: [],
+    avaliacao: 0
+  };
 
   constructor() {}
+
+  present() {
+    this.modal.present();
+  }
+
+  public changeModal(filme) {
+    this.filmeModal.nome = filme.nome;
+    this.filmeModal.foto = filme.foto;
+    this.filmeModal.ano = filme.ano;
+    this.filmeModal.categorias = filme.categorias;
+    this.filmeModal.avaliacao = filme.avaliacao;
+    this.present();
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,115 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  @ViewChild(IonModal) modal: IonModal;
+
+  public seriesPrincipal = [
+    {
+      nome: 'Flash',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lJA2RCMfsWoskqlQhXPSLFQGXEJ.jpg',
+      ano: 2014,
+      categorias: ['Drama', 'Sci-Fi & Fantasy'],
+      avaliacao: 78
+    },
+    {
+      nome: 'Game of Thrones',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/l2ezB41chGDjXcKo24lseuXYtKP.jpg',
+      ano: 2011,
+      categorias: ['Sci-Fi & Fantasy', 'Drama', 'Action & Adventure'],
+      avaliacao: 84
+    },
+    {
+      nome: 'The Good Doctor: O Bom Doutor',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jtLB7xJKcbekmOYkb5NZditBsgk.jpg',
+      ano: 2017,
+      categorias: ['Drama'],
+      avaliacao: 85
+    }
+  ];
+
+  public seriesRoleta1 = [
+    {
+      nome: 'Stranger Things',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/49WJfeN0moxb9IPfGn8AIqMGskD.jpg',
+      ano: 2016,
+      categorias: ['Drama', 'Sci-Fi & Fantasy', 'Mistério'],
+      avaliacao: 86
+    },
+    {
+      nome: 'The Boys',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/stTEycfG9928HYGEISBFaG1ngjM.jpg',
+      ano: 2019,
+      categorias: ['Sci-Fi & Fantasy', 'Action & Adventure'],
+      avaliacao: 85
+    },
+    {
+      nome: 'Peaky Blinders: Sangue, Apostas e Navalhas',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/i0uajcHH9yogXMfDHpOXexIukG9.jpg',
+      ano: 2013,
+      categorias: ['Drama', 'Crime'],
+      avaliacao: 86
+    },
+    {
+      nome: 'Spy x Family',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3r4LYFuXrg3G8fepysr4xSLWnQL.jpg',
+      ano: 2022,
+      categorias: ['Animação', 'Comédia', 'Action & Adventure'],
+      avaliacao: 87
+    },
+  ];
+
+  public seriesRoleta2 = [
+    {
+      nome: 'Eu Não Sou um Animal',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qG59J1Q7rpBc1dvku4azbzcqo8h.jpg',
+      ano: 2004,
+      categorias: ['Animação', 'Comédia'],
+      avaliacao: 92
+    },
+    {
+      nome: 'Arcane',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xQ6GijOFnxTyUzqiwGpVxgfcgqI.jpg',
+      ano: 2021,
+      categorias: ['Animação', 'Sci-Fi & Fantasy', 'Action & Adventure', 'Drama'],
+      avaliacao: 91
+    },
+    {
+      nome: 'The Rising of the Shield Hero',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/baX1tMzHeA7K3eTP4fOvPGwVgAG.jpg',
+      ano: 2019,
+      categorias: ['Animação', 'Action & Adventure', 'Sci-Fi & Fantasy', 'Drama'],
+      avaliacao: 89
+    },
+    {
+      nome: 'Kuroko no Basket',
+      foto: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/5CEIm6RPRU9iy4jBpfreUwJkydv.jpg',
+      ano: 2012,
+      categorias: ['Animação', 'Comédia'],
+      avaliacao: 87
+    },
+  ];
+
+  public serieModal = {
+    nome: '',
+    foto: '',
+    ano: 0,
+    categorias: [],
+    avaliacao: 0
+  };
+
   constructor() {}
+
+  present() {
+    this.modal.present();
+  }
+
+  public changeModal(serie) {
+    this.serieModal.nome = serie.nome;
+    this.serieModal.foto = serie.foto;
+    this.serieModal.ano = serie.ano;
+    this.serieModal.categorias = serie.categorias;
+    this.serieModal.avaliacao = serie.avaliacao;
+    this.present();
+  }
 
 }
