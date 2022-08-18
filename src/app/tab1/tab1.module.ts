103a27aedefe391937badd6ff29e501ff50e343b
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-18 08:00:34 -0300

    Changed to swiper.js

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { SwiperModule } from 'swiper/angular';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -11,7 +12,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    SwiperModule
   ],
   declarations: [Tab1Page]
 })
